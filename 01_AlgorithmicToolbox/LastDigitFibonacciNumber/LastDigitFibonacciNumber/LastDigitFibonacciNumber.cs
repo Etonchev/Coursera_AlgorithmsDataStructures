f15b4b409a8e6b79d5b094f0f9b0051d5effa367
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-09 14:32:06 +0300

    Adds solution to last digit of large fibonacci number assignment

---
+++ 01_AlgorithmicToolbox/LastDigitFibonacciNumber/LastDigitFibonacciNumber/LastDigitFibonacciNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LastDigitFibonacciNumber
+{
+    class LastDigitFibonacciNumber
+    {
+        static void Main()
+        {
+            var fibonacciIndex = long.Parse(Console.ReadLine());
+            if (fibonacciIndex < 0)
+            {
+                throw new ArgumentException("Invalid argument!");
+            }
+
+            var lastDigitFibonacciNumber = CalculateLastDigitFibonacciNumber(fibonacciIndex);
+            Console.WriteLine(lastDigitFibonacciNumber);
+        }
+
+        private static byte CalculateLastDigitFibonacciNumber(long fibonacciIndex)
+        {
+            byte firstFibonacciNumber = 0;
+            byte secondFibonacciNumber = 1;
+            if (fibonacciIndex == 0)
+            {
+                return firstFibonacciNumber;
+            }
+
+            while(fibonacciIndex > 1)
+            {
+                var previous = secondFibonacciNumber;
+                secondFibonacciNumber = (byte)((secondFibonacciNumber + firstFibonacciNumber) % 10);
+                firstFibonacciNumber = previous;
+                fibonacciIndex--;
+            }
+
+            return secondFibonacciNumber;
+        }
+    }
+}
