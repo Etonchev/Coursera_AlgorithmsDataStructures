efc32a04b64096c639e4d7f14f8d1ccd7fe0a784
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-09 14:10:00 +0300

    Adds solution to small fibonacci number assignment

---
+++ 01_AlgorithmicToolbox/SmallFibonacciNumber/SmallFibonacciNumber/SmallFibonacciNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SmallFibonacciNumber
+{
+    class SmallFibonacciNumber
+    {
+        static void Main()
+        {
+            var fibonacciIndex = int.Parse(Console.ReadLine());
+            if(fibonacciIndex < 0)
+            {
+                throw new ArgumentException("Invalid argument!");
+            }
+
+            var fibonacciNumber = CalculateFibonacciNumber(fibonacciIndex);
+            Console.WriteLine(fibonacciNumber);
+        }
+
+        private static long CalculateFibonacciNumber(int fibonacciIndex)
+        {
+            long firstFibonacciNumber = 0;
+            long secondFibonacciNumber = 1;
+            if(fibonacciIndex == 0)
+            {
+                return firstFibonacciNumber;
+            }
+
+            if(firstFibonacciNumber == 1)
+            {
+                return secondFibonacciNumber;
+            }
+
+            while(fibonacciIndex > 1)
+            {
+                var previous = secondFibonacciNumber;
+                secondFibonacciNumber += firstFibonacciNumber;
+                firstFibonacciNumber = previous;
+                fibonacciIndex--;
+            }
+
+            return secondFibonacciNumber;
+        }
+    }
+}
