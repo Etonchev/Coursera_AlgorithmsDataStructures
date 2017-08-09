c8444a7762d4a6f8b4073a87067d0ec39171f460
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-09 16:35:57 +0300

    Adds solution to least common multiple problem

---
+++ 01_AlgorithmicToolbox/LeastCommonMultiple/LeastCommonMultiple/LeastCommonMultiple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeastCommonMultiple
+{
+    class LeastCommonMultiple
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var firstNumber = long.Parse(input[0]);
+            var secondNumber = long.Parse(input[1]);
+            if (firstNumber <= 0 || secondNumber <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            var leastCommonMultiple = CalculateLeastCommonMultiple(firstNumber, secondNumber);
+            Console.WriteLine(leastCommonMultiple);
+        }
+
+        private static long CalculateLeastCommonMultiple(long firstNumber, long secondNumber)
+        {
+            var greatestCommonDivisor = CalculateGreatestCommonDivisor(firstNumber, secondNumber);
+            var leastCommonMultiple = (firstNumber * secondNumber) / greatestCommonDivisor;
+
+            return leastCommonMultiple;
+        }
+
+        private static long CalculateGreatestCommonDivisor(long firstNumber, long secondNumber)
+        {
+            if (secondNumber > firstNumber)
+            {
+                firstNumber += secondNumber;
+                secondNumber = firstNumber - secondNumber;
+                firstNumber = firstNumber - secondNumber;
+            }
+
+            while (secondNumber != 0)
+            {
+                var previous = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = previous;
+            }
+
+            return firstNumber;
+        }
+    }
+}
