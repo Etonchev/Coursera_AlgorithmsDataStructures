1ee63228647f9b60a2e22f025c7890c8b65ad068
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-12 11:16:46 +0300

    Adds solution to changing money assignment

---
+++ 01_AlgorithmicToolbox/ChangingMoney/ChangingMoney/ChangingMoney.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ChangingMoney
+{
+    class ChangingMoney
+    {
+        static void Main()
+        {
+            var amount = int.Parse(Console.ReadLine());
+            if(amount < 1 || amount > 1000)
+            {
+                throw new ArgumentException();
+            }
+
+            var availableCoins = new byte[]{ 10, 5, 1 };
+            var minimumNumberOfCoins = CalculateMinimumNumberOfCoins(amount, availableCoins);
+            Console.WriteLine(minimumNumberOfCoins);
+        }
+
+        private static byte CalculateMinimumNumberOfCoins(int amount, byte[] availableCoins)
+        {
+            availableCoins = availableCoins.OrderByDescending(c => c).ToArray();
+            byte numberOfCoins = 0;
+            foreach (var coin in availableCoins)
+            {
+                if(amount == 0)
+                {
+                    break;
+                }
+
+                numberOfCoins += (byte)(amount / coin);
+                amount %= coin;
+            }
+
+            return numberOfCoins;
+        }
+    }
+}
