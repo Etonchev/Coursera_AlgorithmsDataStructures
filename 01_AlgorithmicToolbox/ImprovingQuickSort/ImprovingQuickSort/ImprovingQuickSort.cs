7859258e5d5e6832c8154a48b01899dcdbb1379d
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-15 15:29:14 +0300

    Adds solution to quick sort assignment

---
+++ 01_AlgorithmicToolbox/ImprovingQuickSort/ImprovingQuickSort/ImprovingQuickSort.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ImprovingQuickSort
+{
+    public static class ArrayExtensions
+    {
+        public static void QuickSort(this long[] array)
+        {
+            RecursiveQuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void RecursiveQuickSort(long[] array, int left, int right)
+        {
+            if (left > right)
+            {
+                return;
+            }
+
+            var pivotIndexes = Partition(array, left, right);
+            if (pivotIndexes.Item1 - 1 - left > 0)
+            {
+                RecursiveQuickSort(array, left, pivotIndexes.Item1 - 1);
+            }
+            if (right - pivotIndexes.Item2 + 1 > 0)
+            {
+                RecursiveQuickSort(array, pivotIndexes.Item2 + 1, right);
+            }
+        }
+
+        private static Tuple<int, int> Partition(long[] array, int left, int right)
+        {
+            var pivot = CalculateMedian(ref array, left, right);
+            var leftmost = left;
+            var rightmost = right;
+            var i = left;
+            while (i <= rightmost) 
+            {
+                if (array[i] < pivot)
+                {
+                    Swap(array, i++, leftmost++);
+                }
+                else if (array[i] > pivot)
+                {
+                    Swap(array, i, rightmost--);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return new Tuple<int, int>(leftmost, rightmost);
+        }
+
+        private static long CalculateMedian(ref long[] array, int left, int right)
+        {
+            var middle = (left + right) / 2;
+            if (array[left].CompareTo(array[middle]) == 1)
+            {
+                Swap(array, left, middle);
+            }
+            if (array[left].CompareTo(array[right]) == 1)
+            {
+                Swap(array, left, right);
+            }
+            if (array[middle].CompareTo(array[right]) == 1)
+            {
+                Swap(array, middle, right);
+            }
+
+            return array[middle];
+        }
+
+        private static void Swap(long[] array, int firstIndex, int secondIndex)
+        {
+            var previous = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = previous;
+        }
+    }
+
+    class ImprovingQuickSort
+    {
+        static void Main()
+        {
+            var arraySize = int.Parse(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            array.QuickSort();
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ 01_AlgorithmicToolbox/ImprovingQuickSort/README.md
@@ -0,0 +1,47 @@
+﻿### Problem Introduction
+The goal in this problem is to redesign a given implementation of the randomized
+quick sort algorithm so that it works fast even on sequences containing
+many equal elements.
+
+### Problem Description
+
+**Task.** To force the given implementation of the quick sort algorithm to efficiently process sequences with
+few unique elements, your goal is replace a 2-way partition with a 3-way partition. That is, your new
+partition procedure should partition the array into three parts: < 𝑥 part, = 𝑥 part, and > x part.
+
+**Input Format.** The first line of the input contains an integer *𝑛*. The next line contains a sequence of *𝑛*
+integers *𝑎<sub>0</sub>*, *𝑎<sub>1</sub>*, . . . , *𝑎<sub>𝑛−1</sub>*.
+
+**Constraints.** 1 ≤ *𝑛* ≤ 10<sup>5</sup>; 1 ≤ *𝑎<sub>𝑖</sub>* ≤ 10<sup>9</sup> for all 0 ≤ *𝑖* < *𝑛*.
+
+**Output Format.** Output this sequence sorted in non-decreasing order.
+
+**Time Limits.** 
+
+|language | C | C++ | Java | Python | C# | Haskell | JavaScript | Ruby | Scala |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+|time (sec) | 2 | 2 | 3 | 10 | 3 | 4 | 10 | 10 | 6 |
+
+**Memory Limit.** 512MB.
+
+**Sample 1**
+
+|Input|
+|-----|
+|5|
+|2 3 9 2 2|
+
+|Output|
+|------|
+|2 2 2 3 9|
+
+**Sample 2**
+
+|Input|
+|-----|
+|9|
+|8 7 7 3 1 3 5 7 4|
+
+|Output|
+|------|
+|1 3 3 4 5 7 7 7 8|
