2a80d979b132434cb68d2c25299ec34d8b4c6aef
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-16 09:38:01 +0300

    Adds solution to inversions to sorted assignment

---
+++ 01_AlgorithmicToolbox/InversionsToSorted/InversionsToSorted/InversionsToSorted.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace InversionsToSorted
+{
+    class InversionsToSorted
+    {
+        public static long MergeSort(long[] array)
+        {
+            var inversions = MergeSortRecursive(array, 0, array.Length - 1);
+
+            return inversions;
+        }
+
+        public static long MergeSortRecursive(long[] array, long left, long right)
+        {
+            long inversions = 0;
+            if (left < right)
+            {
+                long middle = (left + right) / 2;
+                inversions = MergeSortRecursive(array, left, middle);
+                inversions += MergeSortRecursive(array, middle + 1, right);
+                inversions += PerformMerge(array, left, middle, right);
+            }
+
+            return inversions;
+        }
+
+        public static long PerformMerge(long[] array, long left, long middle, long right)
+        {
+            var inversions = 0L;
+            var firstLength = middle - left + 1;
+            var secondLength = right - middle;
+            long[] firstArray = new long[firstLength + 1];
+            long[] secondArray = new long[secondLength + 1];
+
+            for (long i = 0; i < firstLength; i++)
+            {
+                firstArray[i] = array[left + i];
+            }
+            for (long j = 0; j < secondLength; j++)
+            {
+                secondArray[j] = array[middle + 1 + j];
+            }
+
+            //When one of the arrays becomes empty, before the other, the remaining elements will be compared with MaxValue
+            firstArray[firstLength] = Int64.MaxValue;
+            secondArray[secondLength] = Int64.MaxValue;
+
+            for (long k = left, i = 0, j = 0; k <= right; k++)
+            {
+                if (firstArray[i] <= secondArray[j])
+                {
+                    array[k] = firstArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = secondArray[j];
+                    j++;
+                    inversions += firstLength - i;
+                }
+            }
+
+            return inversions;
+        }
+
+        static void Main()
+        {
+            var arraySize = long.Parse(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var inversions = MergeSort(array);
+            Console.WriteLine(inversions);
+        }
+    }
+}
---
+++ 01_AlgorithmicToolbox/InversionsToSorted/README.md
@@ -0,0 +1,36 @@
+﻿### Problem Introduction
+
+An inversion of a sequence *𝑎<sub>0</sub>*, *𝑎<sub>1</sub>*, . . . , *𝑎<sub>𝑛−1</sub>* is a pair of indices 0 ≤ *𝑖* < *𝑗* < *𝑛* such
+that *𝑎<sub>𝑖* > *𝑎<sub>𝑗* . The number of inversions of a sequence in some sense measures how
+close the sequence is to being sorted. For example, a sorted (in non-descending
+order) sequence contains no inversions at all, while in a sequence sorted in descending
+order any two elements constitute an inversion (for a total of *𝑛*(*𝑛* − 1)/2 inversions).
+
+### Problem Description
+
+**Task.** The goal in this problem is to count the number of inversions of a given sequence.
+Input Format. The first line contains an integer *𝑛*, the next one contains a sequence of integers
+*𝑎<sub>0</sub>*, *𝑎<sub>1</sub>*, . . . , *𝑎<sub>𝑛−1</sub>*.
+
+**Constraints.** 1 ≤ *𝑛* ≤ 10<sup>5</sup>, 1 ≤ *𝑎<sub>𝑖</sub>* ≤ 10<sup>9</sup> for all 0 ≤ *𝑖* < *𝑛*.
+
+**Output Format.** Output the number of inversions in the sequence.**Time Limits.** 
+
+|language | C | C++ | Java | Python | C# | Haskell | JavaScript | Ruby | Scala |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+|time (sec) | 3 | 3 | 4.5 | 15 | 4.5 | 6 | 15 | 15 | 9 |
+
+**Memory Limit.** 512MB.
+
+**Sample 1**
+
+|Input|
+|-----|
+|5|
+|2 3 9 2 9|
+
+|Output|
+|------|
+|2|
+
+Explanation: The two inversions here are (1, 3) (*𝑎<sub>1</sub>* = 3 > 2 = *𝑎<sub>3</sub>*) and (2, 3) (*𝑎<sub>2</sub>* = 9 > 2 = *𝑎<sub>3</sub>*)**Sample 2**|Input||-----||6||9 8 7 3 2 1||Output||------||15|
