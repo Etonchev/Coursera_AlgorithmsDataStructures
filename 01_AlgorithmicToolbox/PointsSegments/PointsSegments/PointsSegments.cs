60fdad8a0796929b239456ff7cf19894dfa2da15
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-16 21:49:52 +0300

    Adds solution to points segnments assignment

---
+++ 01_AlgorithmicToolbox/PointsSegments/PointsSegments/PointsSegments.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PointsSegments
+{
+    public class Segment
+    {
+        public long Start { get; set; }
+        public long End { get; set; }
+    }
+
+    public enum SegmentPart
+    {
+        Start,
+        End
+    }
+
+    class PointsSegments
+    {
+        public static long[] NaiveImplementation(Segment[] segments, long[] points)
+        {
+            var result = new long[points.LongLength];
+            for (long i = 0; i < points.LongLength; i++)
+            {
+                var count = 0;
+                foreach (var segment in segments)
+                {
+                    if (points[i] >= segment.Start && points[i] <= segment.End)
+                    {
+                        count++;
+                    }
+                }
+
+                result[i] = count;
+            }
+
+            return result;
+        }
+
+        public static Dictionary<long, long> CalculateNumberOfInclusions(Segment[] segments, long[] points)
+        {
+            var pointInclusion = new Dictionary<long, long>();
+            var pairs = new List<KeyValuePair<string, long>>();
+            foreach (var segment in segments)
+            {
+                pairs.Add(new KeyValuePair<string, long>("left", segment.Start));
+                pairs.Add(new KeyValuePair<string, long>("right", segment.End));
+            }
+            foreach (var point in points)
+            {
+                pairs.Add(new KeyValuePair<string, long>("point", point));
+                if (!pointInclusion.ContainsKey(point))
+                {
+                    pointInclusion.Add(point, 0);
+                }
+            }
+
+            var pairsSorted = pairs.OrderBy(p => p.Value).ThenBy(p => p.Key).ToList();
+            var unclosedParenthesis = 0L;
+            for (int i = 0; i < pairsSorted.Count; i++)
+            {
+                switch (pairsSorted[i].Key)
+                {
+                    case "left":
+                        {
+                            unclosedParenthesis++;
+                            break;
+                        }
+                    case "right":
+                        {
+                            unclosedParenthesis--;
+                            break;
+                        }
+                    case "point":
+                        {
+                            if (unclosedParenthesis > 0)
+                            {
+                                pointInclusion[pairsSorted[i].Value] = unclosedParenthesis;
+                            }
+                            else
+                            {
+                                pointInclusion[pairsSorted[i].Value] = 0;
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            throw new Exception("Invalid pair");
+                        }
+                }
+            }
+
+            return pointInclusion;
+        }
+
+        static void Main()
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var numberOfSegments = input[0];
+            var segments = new Segment[numberOfSegments];
+            for (long i = 0; i < numberOfSegments; i++)
+            {
+                input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+                segments[i] = new Segment();
+                segments[i].Start = input[0];
+                segments[i].End = input[1];
+            }
+
+            var points = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var result = CalculateNumberOfInclusions(segments, points);
+            foreach (var point in points)
+            {
+                Console.Write($"{result[(int)point]} ");
+            }
+        }
+    }
+}
---
+++ 01_AlgorithmicToolbox/PointsSegments/README.md
@@ -0,0 +1,101 @@
+﻿### Problem Introduction
+
+You are organizing an online lottery. To participate, a person bets on a single
+integer. You then draw several ranges of consecutive integers at random.
+A participant’s payoff then is proportional to the number of ranges that
+contain the participant’s number minus the number of ranges that does not
+contain it. You need an efficient algorithm for computing the payoffs for all
+participants. A naive way to do this is to simply scan, for all participants, the
+list of all ranges. However, you lottery is very popular: you have thousands
+of participants and thousands of ranges. For this reason, you cannot afford
+a slow naive algorithm.
+
+### Problem Description
+
+**Task.** You are given a set of points on a line and a set of segments on a line. The goal is to compute, for
+each point, the number of segments that contain this point.
+
+**Input Format.** The first line contains two non-negative integers *𝑠* and *𝑝* defining the number of segments
+and the number of points on a line, respectively. The next *𝑠* lines contain two integers *𝑎<sub>𝑖</sub>*, *𝑏<sub>𝑖</sub>* defining
+the *𝑖*-th segment [*𝑎<sub>𝑖</sub>*, *𝑏<sub>𝑖</sub>*]. The next line contains *𝑝* integers defining points *𝑥<sub>1</sub>*, *𝑥<sub>2</sub>*, . . . , *𝑥<sub>𝑝</sub>*.
+
+**Constraints.** 1 ≤ *𝑠*, *𝑝* ≤ 50000; −10<sup>8</sup> ≤ *𝑎<sub>𝑖</sub>* ≤ *𝑏<sub>𝑖</sub>* ≤ 10<sup>8</sup> for all 0 ≤ *𝑖* < *𝑠*; −10<sup>8</sup> ≤ *𝑥<sub>𝑗</sub>* ≤ 10<sup>8</sup> for all 0 ≤ *𝑗* < *𝑝*.
+
+**Output Format.** Output 𝑝 non-negative integers *𝑘<sub>0</sub>*, *𝑘<sub>1</sub>*, . . . , *𝑘<sub>𝑝−1</sub>* where *𝑘<sub>𝑖</sub>* is the number of segments which
+contain *𝑥<sub>𝑖</sub>*. More formally, *𝑘<sub>𝑖</sub>* = |{*𝑗* : *𝑎<sub>𝑗</sub>* ≤ *𝑥<sub>𝑖</sub>* ≤ *𝑏<sub>𝑗</sub>*}|
+
+**Time Limits.** 
+
+| language | C | C++ | Java | Python | C# | Haskell | JavaScript | Ruby | Scala |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+| time (sec) | 3 | 3 | 4.5 | 15 | 4.5 | 6 | 15 | 15 | 9 |
+
+**Memory Limit.** 512MB.
+
+**Sample 1**
+
+|Input|
+|-----|
+|2 3|
+|0 5|
+|7 10|
+|1 6 11|
+
+|Output|
+|------|
+|1 0 0|
+
+Explanation:
+Here, we have two segments and three points. The first point lies only in the first segment while the
+remaining two points are outside of all the given segments
+
+**Sample 2**
+
+|Input|
+|-----|
+|1 3|
+|-10 10|
+|-100 100 0|
+
+|Output|
+|------|
+|0 0 1|
+
+**Sample 3**
+
+|Input|
+|-----|
+|3 2|
+|0 5|
+|-3 2|
+|7 10|
+|1 6|
+
+|Output|
+|------|
+|2 0|
+
+**Sample 4**
+
+|Input|
+|-----|
+|2 1|
+|5 5|
+|5 5|
+|5|
+
+|Output|
+|------|
+|2|
+
+**Sample 5**
+
+|Input|
+|-----|
+|1 3|
+|0 10|
+|5 5 5|
+
+|Output|
+|------|
+|1 1 1 |
