c5afb36f8c2abb23d9c711a2675c95fc018bce64
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-17 14:35:00 +0300

    Adds solution to find closest pair of points assignment

---
+++ 01_AlgorithmicToolbox/FindClosestPairOfPoints/FindClosestPairOfPoints/FindClosestPairOfPoints.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindClosestPairOfPoints
+{
+    class FindClosestPairOfPoints
+    {
+        public class Point
+        {
+            public const int Precision = 6;
+            public long XCoordinate { get; set; }
+            public long YCoordinate { get; set; }
+
+            public Point(long x, long y)
+            {
+                XCoordinate = x;
+                YCoordinate = y;
+            }
+
+            public static decimal CalculateDistance(Point firstPoint, Point secondPoint)
+            {
+                var distance = (decimal)Math.Sqrt((firstPoint.XCoordinate - secondPoint.XCoordinate) * (firstPoint.XCoordinate - secondPoint.XCoordinate) +
+                    (firstPoint.YCoordinate - secondPoint.YCoordinate) * (firstPoint.YCoordinate - secondPoint.YCoordinate));
+                var precisionCoefficient = Convert.ToDecimal(Math.Pow(10, Precision));
+                var truncatedDistance = Math.Truncate(distance * precisionCoefficient) / precisionCoefficient;
+
+                return truncatedDistance;
+            }
+        }
+
+        private static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+            var points = new Point[length];
+            for (int i = 0; i < length; i++)
+            {
+                var input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+                points[i] = new Point(x: input[0], y: input[1]);
+            }
+
+            var minimumDistance = CalculateClosestDistance(points);
+            Console.WriteLine(minimumDistance);
+        }
+
+        private static decimal CalculateDistance(Point[] points, decimal minimum = Int32.MaxValue)
+        {
+            for (int i = 0; i < points.Length; ++i)
+            {
+                for (int j = i + 1; j < points.Length && Math.Abs(points[j].YCoordinate - points[i].YCoordinate) < minimum; ++j)
+                {
+                    var distance = Point.CalculateDistance(points[i], points[j]);
+                    minimum = Math.Min(distance, minimum);
+                }
+            }
+
+            return minimum;
+        }
+
+        private static decimal CalculateClosestDistanceRecursive(Point[] points)
+        {
+            if (points.Length <= 3)
+            {
+                return CalculateDistance(points);
+            }
+
+            var middle = points.Length / 2;
+
+            var leftSegmentPoints = new List<Point>();
+            for (int i = 0; i < middle; i++)
+            {
+                leftSegmentPoints.Add(points[i]);
+            }
+            var rightSegmentPoints = new List<Point>();
+            for (int i = middle; i < points.Length; i++)
+            {
+                rightSegmentPoints.Add(points[i]);
+            }
+
+            decimal leftSegmentMinimum = CalculateClosestDistanceRecursive(leftSegmentPoints.ToArray());
+            decimal rightSegmentMinimum = CalculateClosestDistanceRecursive(rightSegmentPoints.ToArray());
+            decimal minimum = Math.Min(leftSegmentMinimum, rightSegmentMinimum);
+            var candidates = new List<Point>();
+            var middlePoint = points[middle];
+
+            foreach (var point in points)
+            {
+                if (Math.Abs(middlePoint.XCoordinate - point.XCoordinate) < minimum)
+                {
+                    candidates.Add(point);
+                }
+            }
+
+            var pointSortedByY = candidates.OrderBy(p => p.YCoordinate).ToArray();
+
+            return Math.Min(minimum, CalculateDistance(pointSortedByY, minimum));
+        }
+
+        public static decimal CalculateClosestDistance(Point[] points)
+        {
+            var pointSortedByX = points.OrderBy(p => p.XCoordinate).ToArray();
+
+            return CalculateClosestDistanceRecursive(pointSortedByX);
+        }
+    }
+}
---
+++ 01_AlgorithmicToolbox/FindClosestPairOfPoints/README.md
@@ -0,0 +1,80 @@
+﻿### Problem Introduction
+
+In this problem, your goal is to find the closest pair of points among the given *𝑛*
+points. This is a basic primitive in computational geometry having applications in,
+for example, graphics, computer vision, traffic-control systems.
+
+### Problem Description
+
+**Task.** Given *𝑛* points on a plane, find the smallest distance between a pair of two (different) points. Recall
+that the distance between points (*𝑥<sub>1</sub>*, *𝑦<sub>1</sub>*) and (*𝑥<sub>2</sub>*, *𝑦<sub>2</sub>*) is equal to sqrt((*𝑥<sub>1</sub>* − *𝑥<sub>2</sub>*)<sup>2</sup> + (*𝑦<sub>1</sub>* − *𝑦<sub>2</sub>*)<sup>2</sup>)
+
+**Input Format.** The first line contains the number *𝑛* of points. Each of the following *𝑛* lines defines a point (*𝑥<sub>𝑖</sub>*, *𝑦<sub>𝑖</sub>*).
+
+**Constraints.** 1 ≤ *𝑛* ≤ 10<sup>5</sup>; −10<sup>9</sup> ≤ *𝑥<sub>𝑖</sub>*, *𝑦<sub>𝑖</sub>* ≤ 10<sup>9</sup> are integers.
+
+**Output Format.** Output the minimum distance. The absolute value of the difference between the answer
+of your program and the optimal value should be at most 10<sup>−3</sup>. To ensure this, output your answer
+with at least four digits after the decimal point (otherwise your answer, while being computed correctly,
+can turn out to be wrong because of rounding issues).
+
+**Time Limits.** 
+
+| language | C | C++ | Java | Python | C# | Haskell | JavaScript | Ruby | Scala |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+| time (sec) | 2 | 2 | 3 | 10 | 3 | 4 | 10 | 10 | 6 |
+
+**Memory Limit.** 512MB.
+
+**Sample 1**
+
+|Input|
+|-----|
+|2|
+|0 0|
+|3 4|
+
+|Output|
+|------|
+|5.0|
+
+Explanation: There are only two points here. The distance between them is 5
+
+**Sample 2**
+
+|Input|
+|-----|
+|4|
+|7 7|
+|1 100|
+|4 8|
+|7 7|
+
+|Output|
+|------|
+|0.0|
+
+Explanation: There are two coinciding points among the four given points. Thus, the minimum distance is zero
+
+**Sample 3**
+
+|Input|
+|-----|
+|11|
+|4 4|
+|-2 -2|
+|-3 -4|
+|-1 3|
+|2 3|
+|-4 0|
+|1 1|
+|-1 -1|
+|3 -1|
+|-4 2|
+|-2 4|
+
+|Output|
+|------|
+|1.414213|
+
+Explanation: The smallest distance is sqrt(2). There are two pairs of points at this distance: (−1, −1) and (−2, −2); (−2, 4) and (−1, 3)
