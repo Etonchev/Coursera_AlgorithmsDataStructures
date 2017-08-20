f84e33064e9d6505dfaa7c90109c55c5827c7624
Emil Tonchev <emil.krassimirov@gmail.com>
2017-08-20 22:37:45 +0300

    Adds solution to knapsack without repetition assignment

---
+++ 01_AlgorithmicToolbox/KnapsackWithoutRepetition/KnapsackWithoutRepetition/KnapsackWithoutRepetition.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace KnapsackWithoutRepetition
+{
+    public class KnapsackWithoutRepetition
+    {
+        private static void Main()
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var capacity = input[0];
+            var goldBars = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            var knapsack = new Knapsack(capacity, goldBars);
+            var maximumGold = knapsack.FindMaximumGold();
+            Console.WriteLine(maximumGold);
+        }
+
+        public class Knapsack
+        {
+            public long[,] SolutionMatrix { get; set; }
+            public long Capacity { get; set; }
+            public long[] GoldBars { get; set; }
+
+            public Knapsack(long capacity, long[] goldBars)
+            {
+                Capacity = capacity;
+                GoldBars = goldBars;
+                SolutionMatrix = new long[capacity + 1, goldBars.Length + 1];
+            }
+
+            public long FindMaximumGold()
+            {
+                var maximumGold = FindMaximumGoldRecursive(GoldBars, Capacity, 0);
+
+                return maximumGold;
+            }
+
+            private long FindMaximumGoldRecursive(long[] goldBars, long capacity, long currentItemIndex)
+            {
+                if (currentItemIndex > goldBars.Length - 1)
+                {
+                    return 0;
+                }
+
+                if (capacity < goldBars[currentItemIndex])
+                {
+                    if (SolutionMatrix[capacity, currentItemIndex + 1] == 0)
+                    {
+                        SolutionMatrix[capacity, currentItemIndex + 1] = 
+                            FindMaximumGoldRecursive(goldBars, capacity, currentItemIndex + 1);
+                    }
+
+                    return SolutionMatrix[capacity, currentItemIndex + 1];
+                }
+
+                if (SolutionMatrix[capacity, currentItemIndex + 1] == 0)
+                {
+                    SolutionMatrix[capacity, currentItemIndex + 1] =
+                            FindMaximumGoldRecursive(goldBars, capacity, currentItemIndex + 1);
+                }
+
+                if (SolutionMatrix[capacity - goldBars[currentItemIndex], currentItemIndex + 1] == 0)
+                {
+                    SolutionMatrix[capacity - goldBars[currentItemIndex], currentItemIndex + 1] =
+                        FindMaximumGoldRecursive(goldBars, capacity - goldBars[currentItemIndex], currentItemIndex + 1);
+                }
+
+                return Math.Max(SolutionMatrix[capacity, currentItemIndex + 1],
+                    goldBars[currentItemIndex] + SolutionMatrix[capacity - goldBars[currentItemIndex], currentItemIndex + 1]);
+            }
+        }
+    }
+}
---
+++ 01_AlgorithmicToolbox/KnapsackWithoutRepetition/README.md
@@ -0,0 +1,44 @@
+﻿### Problem Introduction
+
+This problem is about implementing an algorithm for the knapsack without repetitions problem.
+
+### Problem Description
+
+**Task.** In this problem, you are given a set of bars of gold and your goal is to take as much gold as possible
+into your bag. There is just one copy of each bar and for each bar you can either take it or not (hence
+you cannot take a fraction of a bar).
+
+**Input Format.** The first line of the input contains the capacity W of a knapsack and the number *n* of bars
+of gold. The next line contains n integers *w<sub>0</sub>*, *w<sub>1</sub>*, . . . , *w<sub>n−1</sub>* defining the weights of the bars of gold.
+
+**Constraints.** 1 ≤ *W* ≤ 10<sup>4</sup>; 1 ≤ *n* ≤ 300; 0 ≤ *w<sub>0</sub>*, . . . , *w<sub>n−1</sub>* ≤ 10<sup>5</sup>
+
+**Output Format.** Output the maximum weight of gold that fits into a knapsack of capacity W.**Time Limits.** 
+
+| language | C | C++ | Java | Python | C# | Haskell | JavaScript | Ruby | Scala |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+| time (sec) | 2 | 2 | 3 | 10| 3 | 4 | 10 | 10 | 6 |
+
+**Memory Limit.** 512MB.
+
+**Sample 1**
+
+|Input|
+|-----|
+|10 3|
+|1 4 8|
+
+|Output|
+|------|
+|9|
+
+Explanation: Here, the sum of the weights of the first and the last bar is equal to 9.**Sample 2**
+
+|Input|
+|-----|
+|20 4|
+|5 7 12 18|
+
+|Output|
+|------|
+|19|
